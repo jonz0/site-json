44778ea5c08e600faad5d26b341d5cdc105f6667
Jon Lu <13862078+jonz0@users.noreply.github.com>
2023-10-28 04:26:34 -0400

    Update site-javascript.js

--- site-javascript.js
+++ site-javascript.js
@@ -473,121 +473,10 @@ particlesJS(
     }
 );
 
-// particlesJS(
-//     "particles-js-5", {
-//         "particles": {
-//             "number": {
-//                 "value": 20,
-//                 "density": {
-//                     "enable": true,
-//                     "value_area": 800
-//                 }
-//             },
-//             "color": {
-//                 "value": "#edfaff"
-//             },
-//             "shape": {
-//                 "type": "polygon",
-//                 "stroke": {
-//                     "width": 0,
-//                     "color": "#000"
-//                 },
-//                 "polygon": {
-//                     "nb_sides": 12
-//                 },
-//                 "image": {
-//                     "src": "img/github.svg",
-//                     "width": 100,
-//                     "height": 100
-//                 }
-//             },
-//             "opacity": {
-//                 "value": 0.3,
-//                 "random": true,
-//                 "anim": {
-//                     "enable": false,
-//                     "speed": 1,
-//                     "opacity_min": 0.1,
-//                     "sync": false
-//                 }
-//             },
-//             "size": {
-//                 "value": 15,
-//                 "random": true,
-//                 "anim": {
-//                     "enable": true,
-//                     "speed": 10,
-//                     "size_min": 40,
-//                     "sync": false
-//                 }
-//             },
-//             "line_linked": {
-//                 "enable": false,
-//                 "distance": 200,
-//                 "color": "#ffffff",
-//                 "opacity": 1,
-//                 "width": 2
-//             },
-//             "move": {
-//                 "enable": true,
-//                 "speed": 2,
-//                 "direction": "none",
-//                 "random": true,
-//                 "straight": false,
-//                 "out_mode": "out",
-//                 "bounce": false,
-//                 "attract": {
-//                     "enable": true,
-//                     "rotateX": 600,
-//                     "rotateY": 1200
-//                 }
-//             }
-//         },
-//         "interactivity": {
-//             "detect_on": "canvas",
-//             "events": {
-//                 "onhover": {
-//                     "enable": false,
-//                     "mode": "grab"
-//                 },
-//                 "onclick": {
-//                     "enable": false,
-//                     "mode": "push"
-//                 },
-//                 "resize": true
-//             },
-//             "modes": {
-//                 "grab": {
-//                     "distance": 400,
-//                     "line_linked": {
-//                         "opacity": 1
-//                     }
-//                 },
-//                 "bubble": {
-//                     "distance": 400,
-//                     "size": 40,
-//                     "duration": 2,
-//                     "opacity": 8,
-//                     "speed": 3
-//                 },
-//                 "repulse": {
-//                     "distance": 200,
-//                     "duration": 0.4
-//                 },
-//                 "push": {
-//                     "particles_nb": 4
-//                 },
-//                 "remove": {
-//                     "particles_nb": 2
-//                 }
-//             }
-//         },
-//         "retina_detect": true
-//     }
-// );
-
 particlesJS(
-    "particles-js-6", {
+    "particles-js-5",
+
+    {
         "particles": {
             "number": {
                 "value": 45,
@@ -597,16 +486,16 @@ particlesJS(
                 }
             },
             "color": {
-                "value": "#edfaff"
+                "value": "#e72326"
             },
             "shape": {
-                "type": "polygon",
+                "type": "circle",
                 "stroke": {
                     "width": 0,
-                    "color": "#e72326"
+                    "color": "#000000"
                 },
                 "polygon": {
-                    "nb_sides": 12
+                    "nb_sides": 5
                 },
                 "image": {
                     "src": "img/github.svg",
@@ -615,8 +504,8 @@ particlesJS(
                 }
             },
             "opacity": {
-                "value": 0.3,
-                "random": true,
+                "value": 0.5,
+                "random": false,
                 "anim": {
                     "enable": false,
                     "speed": 1,
@@ -628,29 +517,29 @@ particlesJS(
                 "value": 5,
                 "random": true,
                 "anim": {
-                    "enable": true,
-                    "speed": 10,
-                    "size_min": 40,
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
                     "sync": false
                 }
             },
             "line_linked": {
                 "enable": false,
-                "distance": 200,
+                "distance": 150,
                 "color": "#ffffff",
-                "opacity": 1,
-                "width": 2
+                "opacity": 0.4,
+                "width": 1
             },
             "move": {
                 "enable": true,
                 "speed": 2,
-                "direction": "none",
+                "direction": "top",
                 "random": true,
                 "straight": false,
                 "out_mode": "out",
                 "bounce": false,
                 "attract": {
-                    "enable": true,
+                    "enable": false,
                     "rotateX": 600,
                     "rotateY": 1200
                 }
@@ -661,7 +550,7 @@ particlesJS(
             "events": {
                 "onhover": {
                     "enable": false,
-                    "mode": "grab"
+                    "mode": "repulse"
                 },
                 "onclick": {
                     "enable": false,
